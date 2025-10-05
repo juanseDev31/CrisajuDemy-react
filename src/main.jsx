f6d9d188c17e6515002b5017549268e26f226c07
Juan Sebastian Galeano <juansedev31@gmail.com>
2025-10-04 22:40:32 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 // Estilos globales — importa Bootstrap y FontAwesome aquí
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
